129f188c362399823461310bc2fb20e9d5dc84a7
burhanuddin-123 <58836957+burhanuddin-123@users.noreply.github.com>
2021-07-19 20:27:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
-from flask import Flask, render_template
+<html>
+	<head>
+		<style>
+			body{
+				margin: 0;
+			}
+			canvas{
+				width: 100%;
+				height: 100%
+			}
+			.container-fluid{
+				background-color: rgb(124, 156, 229);
+			}
+		</style>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/three.js/r69/three.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="container" style="margin-top: -20px;">
+<span>
+<script src="{{url_for('static', filename='panaroma.js')}}"></script>
+</span>
+</div>
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/register')
-def register():
-    return render_template('newform.html')
-    # return render_template('registration.html')
-
-
-# @app.route('/login')
-# def login():
-#     return render_template('login.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+<!--		<div class="container">-->
+<!--			<div class="col-sm-4">-->
+<!--				<script src="{{url_for('static', filename='panaroma.js')}}"></script>-->
+<!--			</div>-->
+<!--		</div>-->
+	<h1>Hello world</h1>
+	<h1>Hello world</h1>
+	<h1>Hello world</h1>
+	<h1>Hello world</h1>
+	<h1>Hello world</h1>
+	<h1>Hello world</h1>
+	<h1>Hello world</h1>
+</body>
+</html>
