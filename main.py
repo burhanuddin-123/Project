ab0b7cb56db1fa2ca3a6dadd35a80d98f8f8d328
burhanuddin-123 <58836957+burhanuddin-123@users.noreply.github.com>
2021-07-23 20:53:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,73 @@
-<html>
-	<head>
-		<style>
-			body{
-				margin: 0;
-			}
-			canvas{
-				width: 100%;
-				height: 100%
-			}
-			.container-fluid{
-				background-color: rgb(124, 156, 229);
-			}
-		</style>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/three.js/r69/three.min.js"></script>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	</head>
-<body>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
-  </div>
-</nav>
-<div class="container" style="margin-top: -20px;">
-<span>
-<script src="{{url_for('static', filename='panaroma.js')}}"></script>
-</span>
-</div>
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL, MySQLdb
+import bcrypt
 
-<!--		<div class="container">-->
-<!--			<div class="col-sm-4">-->
-<!--				<script src="{{url_for('static', filename='panaroma.js')}}"></script>-->
-<!--			</div>-->
-<!--		</div>-->
-	<h1>Hello world</h1>
-	<h1>Hello world</h1>
-	<h1>Hello world</h1>
-	<h1>Hello world</h1>
-	<h1>Hello world</h1>
-	<h1>Hello world</h1>
-	<h1>Hello world</h1>
-</body>
-</html>
+app = Flask(__name__)
+
+#Configure Db
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'Registration'
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+    if request.method == 'POST':
+        #Fetching data
+        details = request.form
+        name = details['name']
+        email = details['email']
+        password = details['password'].encode('utf-8')
+
+        #hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+        cur = mysql.connection.cursor()
+        cur.execute(f"INSERT INTO users (name,email,password) VALUES (%s, %s, %s)",(name, email, password))
+
+        # getting error :- work upon it ---
+        #cur.execute(f"INSERT INTO users (name,email,password) VALUES (%s, %s, %s)", (name, email, hash_password))
+        mysql.connection.commit()
+        session['name'] = name
+        session['email'] = email
+        cur.close()
+        return redirect(url_for('index'))
+    else:
+        return render_template('registration.html')
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute("SELECT * FROM users WHERE email=%s",(email,))
+        user = cur.fetchone()
+        cur.close()
+
+        if len(user) > 0:
+            #Invalid salt error:- -- work on it
+            # if bcrypt.hashpw(password, user['password'].encode('utf-8')) == user['password'].encode('utf-8'):
+            if user['password'].encode('utf-8') == password:
+                session['name'] = user['name']
+                session['email'] = user['email']
+                return render_template('index.html')
+            else:
+                return "Incorrect password"
+        else:
+            return "Error password or user not match"
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.secret_key = "012#!APaAjaBoleh)(*^%"
+    app.run(debug=True)
